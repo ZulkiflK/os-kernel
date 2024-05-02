ce4875f75e2a84f942316caf88d8c17bcff08826
Sora125 <sora.h717@gmail.com>
2024-05-02 21:55:33 +0100

    Established a freestanding rust binary

---
+++ kernel/target/debug/deps/kernel.d
@@ -0,0 +1,5 @@
+C:\Documents\rust-os\os-kernel\kernel\target\debug\deps\kernel.exe: src\main.rs
+
+C:\Documents\rust-os\os-kernel\kernel\target\debug\deps\kernel.d: src\main.rs
+
+src\main.rs:
---
+++ kernel/target/debug/kernel.d
@@ -0,0 +1,1 @@
+C:\Documents\rust-os\os-kernel\kernel\target\debug\kernel.exe: C:\Documents\rust-os\os-kernel\kernel\src\main.rs
